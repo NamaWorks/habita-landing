f8091823895bdba49d9e1dffca8a2b7abb9364b4
NamaWorks <pedronayamartinez@hotmail.com>
2025-05-12 22:19:20 +0200

    prepare fonts and continue layout mobile first

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,9 +1,10 @@
+import { fontParSizeStyles, fontTitlesFamilyStyles, paddingSStyles } from "../../utils/styleClasses"
 import Button from "../ui/Button"
 
 const Hero = () => {
   return (
     <>
-      <section id="hero-section" className="w-screen bg-red-500">
+      <section id="hero-section" className={`w-screen ${paddingSStyles} bg-[url(public/assets/raw/IMG/JPG/pexels-pnw-prod-9354539.jpg)] bg-cover bg-center`}>
 
         <nav className="flex flex-row justify-between items-start">
           <div className="flex flex-col">
@@ -21,14 +22,14 @@ const Hero = () => {
           </div>
         </nav>
 
-        <div className="flex flex-col items-start w-[40%]">
+        <div className="flex flex-col items-start w-[75%]">
           <div className="flex flex-row w-[100%]">
-          <p className="flex-1">
+          <p className={`flex-1 ${fontParSizeStyles}`}>
             At Habita, we transform ordinary vans into custom-built homes on wheels — ready to take you anywhere.
             <br/>
             Whether you're dreaming of weekend getaways or full-time vanlife, our conversions blend comfort, style, and functionality.
           </p>
-          <ul className="flex-1">
+          <ul className="flex-1 hidden">
             <li>/ Tailored Designs</li>
             <li>/ handcrafted with care</li>
             <li>/ built for life on the road</li>
@@ -38,9 +39,9 @@ const Hero = () => {
           <Button data={{text: 'view the gallery', color:'light'}}/>
         </div>
 
-        <h1>
-          from idea -
-          <br/>
+        <h1 className={`${fontTitlesFamilyStyles} text-[86px]/[80px] text-justify uppercase`}>
+          from idea —
+          {/* <br/> */}
           to open road
         </h1>
 
--- src/utils/styleClasses.ts
+++ src/utils/styleClasses.ts
@@ -1,3 +1,9 @@
 export const colorLightStyles: string = '';
 export const colorDarkStyles: string = '';
+
 export const buttonsCommonStyles: string = '';
+
+export const fontTitlesFamilyStyles: string = `font-(family-name:--font-family-main)`;
+export const fontParSizeStyles: string = `text-[16px]`;
+
+export const paddingSStyles: string = 'p-[16px]';
