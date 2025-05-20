93027a987f582b9691387fbe868a00d48f1dfd6f
NamaWorks <pedronayamartinez@hotmail.com>
2025-05-20 23:38:56 +0200

    prepare mission section

--- src/components/sections/Mission.tsx
+++ src/components/sections/Mission.tsx
@@ -1,17 +1,19 @@
+import { missionImagesStyles } from "../../utils/styleClasses"
+
 const Mission = () => {
   return (
-    <section>
-      <div>
+    <section className={`flex flex-col items-center`}>
+      <div className={`${missionImagesStyles}`}>
         <img src="./../../../public/assets/raw/IMG/GIF/Habita-Gif-Text-01.gif" alt="GIF with several images of camper vans" />
       </div>
 
       <div>
-        <h2 className={`font-(family-name:--font-family-main) text-[48px]/[40px] text-center uppercase text-(color:--color-dark)`}>
+        <h2 className={`font-(family-name:--font-family-main) text-[48px]/[40px] text-center uppercase text-(color:--color-dark) font-[300]`}>
           we transform ordinary vans into custom-built homes on wheels ready to take you anywhere
         </h2>
       </div>
 
-      <div>
+      <div className={`${missionImagesStyles}`}>
         <img src="./../../../public/assets/raw/IMG/GIF/Habita-Gif-Text-02.gif" alt="GIF with several images of camper vans" />
       </div>
     </section>
--- src/utils/styleClasses.ts
+++ src/utils/styleClasses.ts
@@ -1,2 +1,4 @@
 export const colorLightStyles: string = '';
 export const colorDarkStyles: string = '';
+
+export const missionImagesStyles: string = `w-[30%] my-[56px] flex justify-center items-center`;
